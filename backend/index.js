c18e5fdd113127b842bb6a4126546f3a01dc5a24
Jonathan Mutakura <jmutakura@gmail.com>
2022-10-09 10:25:45 -0600

    Finished basic event on a blog post

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const sequalize = require('sequelize');
 const app = express();
 
 app.use(express.json());
 
 const server = app.listen(8000, () => {
-    console.log("server listening on port 8000");
+	console.log('server listening on port 8000');
+});
+
+app.post('/blog-post-event', (req, res) => {
+	console.log(req.body);
 });
