92ea903128d37ffee6e5a1457aa9369e4f602ba0
Jonathan Mutakura <jmutakura@gmail.com>
2022-10-09 11:01:07 -0600

    Finished the main handler implementation for the inserting into blogpost event

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Sequalize = require('sequelize');
+const Sequelize = require('sequelize');
 const app = express();
 
 app.use(express.json());
@@ -12,20 +12,36 @@ const server = app.listen(8000, () => {
 });
 
 app.post('/blog-post-event', async (req, res) => {
-	console.log(req.body);
+	let type;
 
-	const sequalize = new Sequalize(POSTGRESS_CONNECTION_STRING, {});
-	const blogPostId = req.body.event.data.new.id;
-
-	await sequalize.query(
-		'INSERT INTO blog_post_activity(blog_post_id, type) values (:blogPostId, :type);',
-		{
-			replacements: {
-				blogPostId: blogPostId,
-				type: 'created',
-			},
+	if (req.body.event.op === 'INSERT') {
+		type = 'created';
+	} else if (req.body.event.op === 'UPDATE') {
+		if (
+			req.body.event.data.old.is_published === true &&
+			req.body.event.data.new.is_published === false
+		) {
+			type = 'unpublished';
+		} else if (
+			req.body.event.data.old.is_published == false &&
+			req.body.event.data.new.is_published === true
+		) {
+			type = 'published';
 		}
-	);
+	}
 
-	res.status(200).json({ message: 'event registered!' });
+	if (type) {
+		const sequelize = new Sequelize(POSTGRESS_CONNECTION_STRING, {});
+		const blogPostId = req.body.event.data.new.id;
+
+		await sequelize.query(
+			'INSERT INTO blog_post_activity(blog_post_id, type) values (:blogPostId, :type);',
+			{
+				replacements: {
+					blogPostId: blogPostId,
+					type: type,
+				},
+			}
+		);
+	}
 });
